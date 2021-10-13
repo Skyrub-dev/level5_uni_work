85e0d0a2b190a13867a112f54cdd21b072442250
Tom <75093597+Skyrub-dev@users.noreply.github.com>
2021-10-13 14:59:18 +0100

    Add files via upload

---
+++ Week1/Week1/excersize 1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	/*
+	//TASK 1
+	
+	int result = 2;
+
+	for (int i = 1; i < 13; i++)
+	{
+		cout << i << " times 2 = " << i*result << endl;
+	}
+	
+	system( "pause" );*/
+
+	/*
+	//TASK 2
+
+	int input;
+	cout << "Please enter a number ";
+	cin >> input;
+
+	int square;
+	square = input * input;
+
+	cout << " Your number squared is = " << square << endl;
+
+
+	system("pause");*/
+
+	/*
+	//TASK 3
+
+	string name;
+
+	cout << "What's your name? ";
+	cin >> name;
+
+	int length;
+	length = name.length();
+
+	cout << "Your name: " << name << " is " << length << " letters long" << endl;*/
+
+	int number;
+	cout << "Please enter a number: " << endl;
+	cin >> number;
+
+}
