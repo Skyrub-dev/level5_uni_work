57db5c83a692a600bb612783a2179003e44258b1
Tom <75093597+Skyrub-dev@users.noreply.github.com>
2021-10-13 15:25:39 +0100

    Add files via upload

---
+++ Week2/Week2/Week2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //TASK 1
+    //cout << "Welcome\n";
+
+    //TASK 2
+    /*int a = 0;
+    a = 50 + 9;
+    cout << a;*/
+
+    //TASK 3
+    /*cout << "The size of char is " << sizeof(char) << endl;
+    cout << "The size of short is " << sizeof(short) << endl;
+    cout << "The size of int is " << sizeof(int) << endl;
+    cout << "The size of long is " << sizeof(long) << endl;
+    cout << "The size of long long is " << sizeof(long long) << endl;
+    cout << "The size of float is " << sizeof(float) << endl;
+    cout << "The size of double is " << sizeof(double) << endl;
+    cout << "The size of long double is " << sizeof(long double) << endl;
+    cout << "The size of bool is " << sizeof(bool) << endl;*/
+
+    //TASK 4
+    /*int m1 = 5, m2 = 7;
+    double d1 = 3.7, d2 = 8.0;
+
+    cout << m1 + m2 << endl;
+    cout << m1 - m2 << endl;
+    cout << m1 * m2 << endl;
+    cout << m1 / m2 << endl;
+
+    cout << d1 + d2 << endl;
+    cout << d1 - d2 << endl;
+    cout << d1 * d2 << endl;
+    cout << d1 / d2 << endl;
+
+    cout << m1 / d2 << endl;
+    cout << m1 * d2 << endl;
+    cout << m1 + d2 << endl;
+    cout << m1 - d2 << endl;*/
+}
